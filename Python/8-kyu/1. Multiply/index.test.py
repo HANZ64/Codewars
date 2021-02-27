fc2183da2f40c0c52300442e9feab45f6ef25a1d
HANZ64 <hanz64ah@gmail.com>
2021-02-27 23:56:10 +0000

    Completed 8kyu - Multiply

---
+++ Python/8-kyu/1. Multiply/index.test.py
@@ -0,0 +1,7 @@
+Test.assert_equals(multiply(1,1), 1)
+Test.assert_equals(multiply(2,1), 2)
+Test.assert_equals(multiply(2,2), 4)
+Test.assert_equals(multiply(3,5), 15)
+Test.assert_equals(multiply(1.5,2.5), 3.75)
+Test.assert_equals(multiply(0,5), 0)
+Test.assert_equals(multiply(0,0), 0)
