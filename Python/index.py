b9f8b8fc7631d0604f0bbabbf5c9acc81127d4d2
HANZ64 <hanz64ah@gmail.com>
2021-03-01 05:48:19 +0000

    Added template files

---
+++ Python/index.py
@@ -0,0 +1,43 @@
+'''
+  Title:
+    Text
+
+  Kata Link:
+    https://www.codewars.com/kata/text
+
+  Instructions:
+    Text.
+
+  Problem:
+    Code
+'''
+
+#=============================================#
+
+# My solution:
+
+
+
+#=============================================#
+
+# 1. Alternative Solution:
+
+
+
+# 2. Alternative Solution:
+
+
+
+# 3. Alternative Solution:
+
+
+
+# 4. Alternative Solution:
+
+
+
+# 5. Alternative Solution:
+
+
+
+#=============================================#
