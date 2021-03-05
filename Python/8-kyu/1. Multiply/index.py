591431ad32a6828a7f756fbaa1de503d3e0b5822
HANZ64 <hanz64ah@gmail.com>
2021-03-05 00:37:05 +0000

    Completed 8kyu - Multiply

--- Python/8-kyu/1. Multiply/index.py
+++ Python/8-kyu/1. Multiply/index.py
@@ -1,16 +1,16 @@
 '''
-	Title:
-		Multiply
+  Title:
+    Multiply
 
-	Kata Link:
-		https://www.codewars.com/kata/multiply
+  Kata Link:
+    https://www.codewars.com/kata/multiply
 
-	Instructions:
-		This code does not execute properly. Try to figure out why.
+  Instructions:
+    This code does not execute properly. Try to figure out why.
 
-	Problem:
-		def multiply(a, b):
-			a * b
+  Problem:
+    def multiply(a, b):
+        a * b
 '''
 
 #=============================================#
@@ -18,21 +18,21 @@
 # My solution:
 
 def multiply(a, b):
-	return a * b
+    return a * b
 
 #=============================================#
 
 # 1. Alternative Solution:
 
 def multiply(a, b):
-	num = a * b
-	return num
+    num = a * b
+    return num
 
 # 2. Alternative Solution:
 
 def multiply(a, b):
-	a *= b
-	return a
+    a *= b
+    return a
 
 # 3. Alternative Solution:
 
