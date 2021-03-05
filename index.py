ac0ae96e3a150857decd600715f0184aadfcdb59
HANZ64 <hanz64ah@gmail.com>
2021-03-05 00:23:09 +0000

    Added tab & space test file

---
+++ index.py
@@ -0,0 +1,58 @@
+'''
+	Title:
+		Multiply
+
+	Kata Link:
+		https://www.codewars.com/kata/multiply
+
+	Instructions:
+		This code does not execute properly. Try to figure out why.
+
+	Problem:
+		def multiply(a, b):
+			a * b
+'''
+
+#=============================================#
+
+# My solution:
+
+def multiply(a, b):
+	return a * b
+
+#=============================================#
+
+# 1. Alternative Solution:
+
+def multiply(a, b):
+	num = a * b
+	return num
+
+# 2. Alternative Solution:
+
+def multiply(a, b):
+	a *= b
+	return a
+
+# 3. Alternative Solution:
+
+multiply = lambda a, b: a * b
+
+#=============================================#
+
+Tab Test:
+
+0 Tab
+	1 Tab
+		2 Tab
+			3 Tab
+				4 Tab
+
+Space Test:
+
+0 Space
+  2 Space
+    3 Space
+      4 Space
+
+#=============================================#
