ec088d6edae132cdaa48d3b34a6ede268b335331
HANZ64 <hanz64ah@gmail.com>
2021-03-08 20:46:41 +0000

    Completed 8kyu - Remove First and Last Character

--- Python/8-kyu/6. Remove First and Last Character/index.py
+++ Python/8-kyu/6. Remove First and Last Character/index.py
@@ -6,7 +6,8 @@
     https://www.codewars.com/kata/remove-first-and-last-character
 
   Instructions:
-    It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+    It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. 
+    You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
   Problem:
     def remove_char(s):
